1ca43673b97c80e366c427afe2c214f5c5c9e09c
El Mehdi Bouâmama <elmehdi.bouamama@gmail.com>
2023-05-04 15:39:27 +0000

    Added missing file BaseHSAPI.ts

---
+++ src/utils/classes/BaseHSAPI.ts
@@ -0,0 +1,15 @@
+export class BaseHSAPI {
+
+    guildId: string = ''
+    APIKey: string = ''
+
+    constructor(guildId: string, APIKey?: string) {
+        this.guildId = guildId
+        this.APIKey = APIKey ?? ''
+    }
+
+    clearKey() {
+        this.APIKey = ''
+        return true
+    }
+}
